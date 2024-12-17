292012ef60097b5c6ffad5fc65943331bcb66eab
asif adham <asif@alagzoo.com>
2024-12-17 15:27:34 +0600

    added routebased signin page

---
+++ src/components/auth.js
@@ -0,0 +1,29 @@
+import React, { lazy, Suspense, useEffect } from 'react'; // Must be imported for webpack to work
+import Display from '../shared/Display';
+import {useStore} from '../store';
+import { Navigate, useNavigate } from 'react-router-dom';
+
+const AuthApp = lazy(() => import('AuthApp/Auth'));
+
+function Auth() {
+  const { state } = useStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(state.isAuthenticated) {
+      navigate('/product');
+    }
+  }, [state.isAuthenticated])
+
+  return (
+    <div className="App">
+      <Suspense fallback={<div>Loading Auth...</div>}>
+        <Display condition={!state.isAuthenticated}>
+          <AuthApp />
+        </Display>
+      </Suspense>
+    </div>
+  );
+}
+
+export default Auth;
