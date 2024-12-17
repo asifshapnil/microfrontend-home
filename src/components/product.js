506bc8aed2002dec6e76cfdd6d8f382ffcb33ffd
asif adham <asif@alagzoo.com>
2024-12-17 15:27:49 +0600

    added route based productpage

---
+++ src/components/product.js
@@ -0,0 +1,21 @@
+import React, { lazy, Suspense } from 'react'; // Must be imported for webpack to work
+import {useStore} from '../store';
+import Display from '../shared/Display';
+
+const ProductApp = lazy(() => import('ProductApp/Product'));
+
+function Product() {
+  const { state } = useStore();
+
+  return (
+    <div className="App">
+      <Suspense fallback={<div>Loading Products...</div>}>
+        <Display condition={state.isAuthenticated}>
+          <ProductApp />
+        </Display>
+      </Suspense>
+    </div>
+  );
+}
+
+export default Product;
