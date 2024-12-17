78f1629b766b51eed6daee05a20e874934e2166c
asif adham <asif@alagzoo.com>
2024-12-17 15:26:33 +0600

    updated webpack resolving page refresh bugs and added product

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,5 @@
-//home-app/webpack.config.js
-// home-app/webpack.config.js
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// import ModuleFederationPlugin from webpack
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-// import dependencies from package.json, which includes react and react-dom
 const { dependencies } = require("./package.json");
 const path = require('path');
 
@@ -11,8 +7,15 @@ const path = require('path');
 module.exports = {
     entry: "./src/entry.js",
     mode: "development",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name]_bundle.js', // Name the entry chunk dynamically
+        chunkFilename: '[name].[contenthash].js', // Use dynamic names for non-entry chunks
+        publicPath: '/', // Ensure publicPath is set correctly
+    },
     devServer: {
-        port: 3000,  // port 3001 for header-app,
+        port: 3000,  
+        historyApiFallback: true,
     },
     module: {
         rules: [
@@ -39,27 +42,23 @@ module.exports = {
             template: "./public/index.html",
         }),
         new ModuleFederationPlugin({
-            name: "HomeApp",  // This application named 'HomeApp'
-            // This is where we define the federated modules that we want to consume in this app. 
-            // Note that we specify "Header" as the internal name 
-            // so that we can load the components using import("Header/"). 
-            // We also define the location where the remote's module definition is hosted: 
-            // Header@[http://localhost:3001/remoteEntry.js]. 
-            // This URL provides three important pieces of information: the module's name is "Header", it is hosted on "localhost:3001", 
-            // and its module definition is "remoteEntry.js".
+            name: "HomeApp",  
             exposes: {
-                './store': './src/store', // Expose the store module
+                './store': './src/store', 
+                './Display': './src/shared/Display', 
             },
             remotes: {
                 "HeaderApp": "HeaderApp@http://localhost:3001/remoteEntry.js",
+                "AuthApp": "AuthApp@http://localhost:3003/remoteEntry.js",
+                "ProductApp": "ProductApp@http://localhost:3004/remoteEntry.js",
             },
-            shared: {  // and shared
-                ...dependencies,  // other dependencies
-                react: { // react
+            shared: {  
+                ...dependencies,  
+                react: { 
                     singleton: true,
                     requiredVersion: dependencies["react"],
                 },
-                "react-dom": { // react-dom
+                "react-dom": {
                     singleton: true,
                     requiredVersion: dependencies["react-dom"],
                 },
